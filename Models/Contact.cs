b00023fef8d43978f3405dfa3f9cfe89bd11de3d
RogerCodes <rogelioherreradev@gmail.com>
2024-06-20 17:23:35 -0600

    Created Contact Model, made changes to .gitignore

---
+++ Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudExample.Models;
+
+[Table("Contacts")]
+public class Contact
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [DisplayName("Name")]
+    public int Name { get; set; }
+    [Required]
+    [DisplayName("Phone Number")]
+    public string PhoneNumber { get; set; }
+    [Required]
+    [EmailAddress]
+    [DisplayName("E-Mail")]
+    public string Email { get; set; }
+    [DisplayName("Date Added")]
+    public DateTimeOffset DateCreated { get; set; }
+}
